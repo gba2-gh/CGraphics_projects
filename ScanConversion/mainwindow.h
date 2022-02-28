9b1950b7c6587c7cf0d9f4423a7f5a8af0cb2455
gba2 <azaz332733@gmail.com>
2022-02-28 01:06:49 -0600

    scan

---
+++ ScanConversion/mainwindow.h
@@ -0,0 +1,50 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+#include<QPainter>
+#include<QApplication>
+#include<QtWidgets>
+#include"renderWindow.h"
+#include"camProjection.h"
+#include"cubeobject.h"
+
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+class renderWindow;
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+    double currentAngle=0;
+
+public slots:
+    void pickColor();
+    void setOrtho();
+    void setPers();
+    void setRotation();
+    void drawObject();
+
+
+private:
+    Ui::MainWindow *ui;
+    QPushButton *colorBtn;
+    QPushButton *orthoProyBtn;
+    QPushButton *persProyBtn;
+    QPushButton *setRotationBtn;
+    renderWindow *renderwindow;
+    CamProjection *camProj;
+    CamProjection *camProj2;
+    CubeObject *cubeObject;
+    QTimer *timer ;
+    bool orthoProy= false;
+    bool rotateBool=false;
+};
+#endif // MAINWINDOW_H
---
+++ ScanConvesion/renderWindow.h
@@ -0,0 +1,29 @@
+#ifndef RENDERWINDOW_H
+#define RENDERWINDOW_H
+
+#include <QWidget>
+#include <QPainter>
+
+class renderWindow: public QWidget{
+    Q_OBJECT //para poder crear slots
+
+public:
+    QList<int> pointsList;
+    renderWindow(QWidget *parent=0); //constructor
+    void paintEvent(QPaintEvent *event) override;
+
+
+public slots:
+    void setPenColor(QColor color);
+
+
+private:
+    QPen pointPen;
+
+
+
+};
+
+
+
+#endif // RENDERWINDOW_H
